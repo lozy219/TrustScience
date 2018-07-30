0f5b34edb2ed2316d75790c0b81af1cde38bfc7e
Lei Mingyu <leimy@seagroup.com>
2018-07-30 14:30:28 +0800

    refactor rank.js and add todos

--- frontend/rank.js
+++ frontend/rank.js
@@ -1,6 +1,51 @@
 let nicknames = {};
 let filenames;
-let statsArr;
+let statsArr = [];
+
+// function declarations
+
+const sortStats = () => {
+  statsArr.sort((a, b) => {
+    let aData = a[1];
+    let bData = b[1];
+    if (aData.winP < bData.winP) {
+      return 1;
+    } else if (aData.winP > bData.winP) {
+      return -1;
+    } else if (aData.sum > bData) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+};
+
+const clearTable = () => {
+  // [TODO: clear the table content for re-render]
+};
+
+const renderTable = () => {
+  for (let entry of statsArr) {
+    const [name, stat] = entry;
+    const filename = `resources/pixyys/${filenames[name]}.png?_v=1`;
+    const {winning, losing, sum, winP} = stat;
+    $('.result .col').append('<div class=row></div>');
+    $('.avatar .row').last().html(`<img src="${filename}">`);
+    $('.win .row').last().text(winning);
+    $('.lose .row').last().text(losing);
+    $('.sum .row').last().text(sum);
+    $('.winp .row').last().text(parseInt((winning / sum) * 100) + '%');
+  }
+}
+
+const _refresh = () => {
+  sortStats();
+  clearTable();
+  renderTable();
+};
+
+
+// event handlers
 
 $.get('data/nickname.json?_v=7')
   .done(data => {
@@ -17,35 +62,20 @@ $.get('data/filename.json?_v=12')
     filenames = data;
     $.get('data/data.json?_v=7')
       .done(stats => {
+        // populate stas array
         statsArr = Object.entries(stats);
         for (let entry of statsArr) {
           const entryData = entry[1];
           entryData.sum = entryData.losing + entryData.winning;
           entryData.winP = entryData.winning / entryData.sum;
         }
-        statsArr.sort((a, b) => {
-          let aData = a[1];
-          let bData = b[1];
-          if (aData.winP < bData.winP) {
-            return 1;
-          } else if (aData.winP > bData.winP) {
-            return -1;
-          } else if (aData.sum > bData) {
-            return 1;
-          } else {
-            return -1;
-          }
-        });
-        for (let entry of statsArr) {
-          const [name, stat] = entry;
-          const filename = `resources/pixyys/${filenames[name]}.png?_v=1`;
-          const {winning, losing, sum, winP} = stat;
-          $('.result .col').append('<div class=row></div>');
-          $('.avatar .row').last().html(`<img src="${filename}">`);
-          $('.win .row').last().text(winning);
-          $('.lose .row').last().text(losing);
-          $('.sum .row').last().text(sum);
-          $('.winp .row').last().text(parseInt((winning / sum) * 100) + '%');
-        }
+        // refresh UI
+        _refresh();
       });
   });
+
+// $([TODO: table header selector]).on('click', event => {
+//   [TODO: update sort mode]
+//   _refresh();
+// });
+
