5e32a63a5e9f479e85fe463362e4ab89151a3458
Lei Mingyu <uygnim.iel@gmail.com>
2018-09-07 01:12:13 +0800

    update data

--- frontend/rank.js
+++ frontend/rank.js
@@ -70,7 +70,7 @@ const clearTable = () => {
 const renderTable = () => {
   for (let entry of statsArr) {
     const [name, stat] = entry;
-    const filename = `resources/pixyys/${filenames[name]}.png?_v=baizangzhu`;
+    const filename = `resources/pixyys/${filenames[name]}.png?_v=lianyou`;
     const {winning, losing, sum, winP} = stat;
     $('.result .col').append('<div class=row></div>');
     $('.avatar .row').last().html(`<img src="${filename}">`);
@@ -89,7 +89,7 @@ const _refresh = () => {
 
 // event handlers
 
-$.get('data/nickname.json?_v=baizangzhu')
+$.get('data/nickname.json?_v=lianyou')
   .done(data => {
     for (let key of Object.keys(data)) {
       const arr = data[key];
@@ -99,10 +99,10 @@ $.get('data/nickname.json?_v=baizangzhu')
     }
   });
 
-$.get('data/filename.json?_v=baizangzhu')
+$.get('data/filename.json?_v=lianyou')
   .done(data => {
     filenames = data;
-    $.get('data/data.json?_v=baizangzhu')
+    $.get('data/data.json?_v=lianyou')
       .done(stats => {
         // populate stas array
         statsData = stats;
