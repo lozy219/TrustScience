ca5b0b3d93e75a3f2ae2f97d2ed4b18efdcbd526
Lei Mingyu <leimy@seagroup.com>
2019-02-27 21:02:31 +0800

    更新一月数据

--- frontend/main.js
+++ frontend/main.js
@@ -88,7 +88,7 @@ $('.photo').on('click', () => {
 let nicknames = {};
 let filenames;
 
-$.get('frontend/data/nickname.json?_v=baqidashe')
+$.get('frontend/data/nickname.json ?_v=daoheshenyuzhuanjin')
   .done(data => {
     for (let key of Object.keys(data)) {
       const arr = data[key];
@@ -98,12 +98,12 @@ $.get('frontend/data/nickname.json?_v=baqidashe')
     }
   });
 
-$.get('frontend/data/filename.json?_v=baqidashe')
+$.get('frontend/data/filename.json ?_v=daoheshenyuzhuanjin')
   .done(data => {
     filenames = data;
-    $.get('frontend/data/weightedScore.json?_v=baqidashe')
+    $.get('frontend/data/weightedScore.json ?_v=daoheshenyuzhuanjin')
       .done(scores => {
-        $.get('frontend/data/data.json?_v=baqidashe')
+        $.get('frontend/data/data.json ?_v=daoheshenyuzhuanjin')
           .done(content => {
             const stats = content['A'];
             const $input = $('#main');
--- frontend/rank.js
+++ frontend/rank.js
@@ -70,7 +70,7 @@ const clearTable = () => {
 const renderTable = () => {
   for (let entry of statsArr) {
     const [name, stat] = entry;
-    const filename = `resources/pixyys/${filenames[name]}.png?_v=baqidashe`;
+    const filename = `resources/pixyys/${filenames[name]}.png ?_v=daoheshenyuzhuanjin`;
     const {winning, losing, sum, winP} = stat;
     $('.result .col').append('<div class=row></div>');
     $('.avatar .row').last().html(`<img src="${filename}">`);
@@ -89,7 +89,7 @@ const _refresh = () => {
 
 // event handlers
 
-$.get('data/nickname.json?_v=baqidashe')
+$.get('data/nickname.json ?_v=daoheshenyuzhuanjin')
   .done(data => {
     for (let key of Object.keys(data)) {
       const arr = data[key];
@@ -99,10 +99,10 @@ $.get('data/nickname.json?_v=baqidashe')
     }
   });
 
-$.get('data/filename.json?_v=baqidashe')
+$.get('data/filename.json ?_v=daoheshenyuzhuanjin')
   .done(data => {
     filenames = data;
-    $.get('data/data.json?_v=baqidashe')
+    $.get('data/data.json ?_v=daoheshenyuzhuanjin')
       .done(stats => {
         // populate stas array
         statsData = stats;
