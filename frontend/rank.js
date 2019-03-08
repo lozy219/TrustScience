af370ff1daded06bdacab8d94cca5b8d2dc828cc
Lei Mingyu <leimy@seagroup.com>
2019-03-08 22:43:06 +0800

    更新今天数据

--- frontend/rank.js
+++ frontend/rank.js
@@ -70,7 +70,7 @@ const clearTable = () => {
 const renderTable = () => {
   for (let entry of statsArr) {
     const [name, stat] = entry;
-    const filename = `resources/pixyys/${filenames[name]}.png?_v=liujianqiao`;
+    const filename = `resources/pixyys/${filenames[name]}.png?_v=xige`;
     const {winning, losing, sum, winP} = stat;
     $('.result .col').append('<div class=row></div>');
     $('.avatar .row').last().html(`<img src="${filename}">`);
@@ -89,7 +89,7 @@ const _refresh = () => {
 
 // event handlers
 
-$.get('data/nickname.json?_v=liujianqiao')
+$.get('data/nickname.json?_v=xige')
   .done(data => {
     for (let key of Object.keys(data)) {
       const arr = data[key];
@@ -99,10 +99,10 @@ $.get('data/nickname.json?_v=liujianqiao')
     }
   });
 
-$.get('data/filename.json?_v=liujianqiao')
+$.get('data/filename.json?_v=xige')
   .done(data => {
     filenames = data;
-    $.get('data/data.json?_v=liujianqiao')
+    $.get('data/data.json?_v=xige')
       .done(stats => {
         // populate stas array
         statsData = stats;
